9e1450ad40ba511ba313f9fa02ab5ddcbc9c52c6
vvduth <ducthai060501@gmail.com>
2021-10-03 12:16:14 +0300

    CSS modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,44 +2,36 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const  FormControl = styled.div`
+// const  FormControl = styled.div`
 
-margin: 0.5rem 0;
+// margin: 0.5rem 0;
 
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => (props.invalid ? 'red':'black') }
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => (props.invalid ? 'red' : '#ccc') };
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: ${props => (props.invalid ? '#ffd7d7':'transparent')} ;
+//   border-color: #8b005d;
+// }
 
-&.invalid input {
-  border-color: red;
-  background: #ffd7d7;
-}
-
-&.invalid label {
-  color: red;
- 
-}
-`
+// `
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -63,11 +55,11 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className= {!isValid && 'invalid'}>
+      <div className = {`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label >Course Goal</label>
         <input  type="text" 
                 onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
